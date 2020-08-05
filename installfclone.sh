90c757115acff182d25389c60c16e349cff1d813
wh8929 <57432600+wh8929@users.noreply.github.com>
2020-08-05 15:04:23 +0800

    Update installfclone.sh

--- installfclone.sh
+++ installfclone.sh
@@ -35,7 +35,6 @@ case $OSARCH in
         ;;
 esac
 
-apt install unzip -y
 wget -qO- https://api.github.com/repos/mawaya/rclone/releases/latest \
 | grep browser_download_url | grep "$BINTAG" | cut -d '"' -f 4 \
 | wget --no-verbose -i-
